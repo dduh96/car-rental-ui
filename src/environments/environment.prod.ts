0006a5254ded5f68b3185fc7d029842e03d898db
Nichil Strasser <nichil.strasser@me.com>
2022-04-17 22:02:48 +0200

    fix: changed prod basPath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import {InjectionToken} from "@angular/core";
 export const environment = {
   production: true,
   GAPI_KEY: "AIzaSyCI6Lh4SP_YcEmC444sEbpHI3Gnsdi4NIg",
-  BASE_PATH: '/api/v1',
+  BASE_PATH: '/',
   COLLECTION_FORMATS: {
     'csv': ',',
     'tsv': '   ',
