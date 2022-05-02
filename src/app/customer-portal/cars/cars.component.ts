e8774e8ac581b6fb76520fb6896f63e7e37ea24b
Katharina <katharina.buehn@stud.fh-campuswien.ac.at>
2022-05-02 21:07:07 +0200

    get active cars - not tested yet

--- src/app/customer-portal/cars/cars.component.ts
+++ src/app/customer-portal/cars/cars.component.ts
@@ -48,7 +48,10 @@ export class CarsComponent implements OnInit {
   };
 
   constructor(private carService: CarService, private currencyService: CurrencyService) {
-    carService.getCars(Car.CurrencySymbolEnum.Usd).subscribe(res => this.cars = res);
+   // carService.getCars(Car.CurrencySymbolEnum.Usd).subscribe(res => this.cars = res);
+    carService.getCars(Car.CurrencySymbolEnum.Usd).subscribe(res => {
+      this.cars = res.filter(car => car.car_status == Car.CarStatusEnum.Available); //todo: test if works
+    });
     currencyService.getCurrencies().subscribe(res => this.currency = res);
   }
 
