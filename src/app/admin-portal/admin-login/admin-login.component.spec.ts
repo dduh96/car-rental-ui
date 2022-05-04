7af1a0ad04e2c5b07dc0319a25b2594b8b26d91c
daniela duh <daniela.duh@gmail.com>
2022-05-04 22:34:05 +0200

    fix: test imports (again)

--- src/app/admin-portal/admin-login/admin-login.component.spec.ts
+++ src/app/admin-portal/admin-login/admin-login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminLoginComponent } from './admin-login.component';
-import {MatDialog} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
@@ -13,7 +13,14 @@ describe('AdminLoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AdminLoginComponent ],
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule
+      ],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: []},
+      ]
     })
     .compileComponents();
   });
--- src/app/customer-portal/order-login/order-login.component.spec.ts
+++ src/app/customer-portal/order-login/order-login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderLoginComponent } from './order-login.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('OrderLoginComponent', () => {
   let component: OrderLoginComponent;
@@ -10,7 +11,10 @@ describe('OrderLoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ OrderLoginComponent ],
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
