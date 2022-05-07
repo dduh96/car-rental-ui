783ba5f2068dc48ce40dac6e2ae759c0c3122f75
Katharina <katharina.buehn@stud.fh-campuswien.ac.at>
2022-05-07 19:27:05 +0200

    test: cars-component add loadCar test

--- src/app/customer-portal/cars/cars.component.spec.ts
+++ src/app/customer-portal/cars/cars.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
-import { CarsComponent } from './cars.component';
+import {CarsComponent} from './cars.component';
 import {CarsItemComponent} from "./cars-item/cars-item.component";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
@@ -8,14 +8,21 @@ import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-fi
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CarService} from "../../api/car.service";
+import {Observable, of} from "rxjs";
+import {Car} from "../../model/car";
+import {buildCar} from "../../mock/order-mock/buildCar";
+import {HttpEventType, HttpProgressEvent} from "@angular/common/http";
 
 describe('CarsComponent', () => {
   let component: CarsComponent;
   let fixture: ComponentFixture<CarsComponent>;
+  let service: CarService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarsComponent, CarsItemComponent],
+      declarations: [CarsComponent, CarsItemComponent],
+      providers: [CarService],
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
@@ -25,16 +32,30 @@ describe('CarsComponent', () => {
         BrowserAnimationsModule
       ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CarsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(CarService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  const mockHttp = {
+    request: jasmine.createSpy('request')
+  };
+
+  it('should load cars', fakeAsync(() => {
+    mockHttp.request.and.returnValue(
+      of(true)
+    );
+    service.getCarsByStatus(Car.CarStatusEnum.Available).pipe().subscribe( progress =>{
+      expect(progress).toEqual([]);
+      }
+    )
+  }));
 });
